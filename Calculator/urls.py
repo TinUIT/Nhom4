92c2453187e721b140f4d368bc8c313bdbff8534
phamtrongtin <phamtrongtin5@gmail.com>
2022-09-24 21:39:18 +0700

    The first time

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('', include('calc.urls')),
     path('admin/', admin.site.urls),
-    path('', include('static')),
+    path('static/',),
 ]
